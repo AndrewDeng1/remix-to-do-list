42507e26fd2bdfe43df84c0a83aee8115380c5e7
AndrewDeng1 <bndarw2006@gmail.com>
2025-05-03 23:23:10 -0400

    all

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,48 +1,21 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
+import type { EntryContext } from "@remix-run/cloudflare";
 import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import { renderToString } from "react-dom/server";
 
-const ABORT_DELAY = 5_000;
-
-export default async function handleRequest(
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
-  loadContext: AppLoadContext
+  remixContext: EntryContext
 ) {
-  const isBot = isbot(request.headers.get("user-agent") || "");
-
-  const stream = await renderToReadableStream(
-    <RemixServer
-      context={remixContext}
-      url={request.url}
-      abortDelay={ABORT_DELAY}
-    />,
-    {
-      signal: AbortSignal.timeout(ABORT_DELAY),
-      onError(error: unknown) {
-        console.error(error);
-        responseStatusCode = 500;
-      },
-    }
+  let markup = renderToString(
+    <RemixServer context={remixContext} url={request.url} />
   );
-
-  if (isBot) {
-    await stream.allReady;
-  }
 
   responseHeaders.set("Content-Type", "text/html");
 
-  return new Response(stream, {
-    headers: responseHeaders,
+  return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
